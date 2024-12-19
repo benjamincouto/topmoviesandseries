becba0e899930c55c21a566eb369ed32d4d04536
benjamincouto <ben.couto@live.com>
2024-12-19 01:15:59 -0300

    removed previous code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,27 +1,3 @@
-// import ExternalServices from "./ExternalServices.mjs";
-
-// const externalServices = new ExternalServices();
-
-// async function fetchTopMovies() {
-//   try {
-//     const topMovies = await externalServices.getTopMovies();
-//     console.log("Top Movies:", topMovies);
-//   } catch (error) {
-//     console.error("Error:", error);
-//   }
-// }
-
-// async function fetchTopSeries() {
-//   try {
-//     const topSeries = await externalServices.getTopSeries();
-//     console.log("Top Series:", topSeries);
-//   } catch (error) {
-//     console.error("Error:", error);
-//   }
-// }
-
-// fetchTopMovies();
-// fetchTopSeries();
 
 import ExternalServices from "./ExternalServices.mjs";
 
