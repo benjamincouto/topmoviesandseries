55ecc467d0941369ba84b45bc7a87f432f1a274f
benjamincouto <ben.couto@live.com>
2024-12-19 00:03:57 -0300

    added details into vite.config

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
+        details: resolve(__dirname, "src/detail-pages/index.html")
       },
     },
   },
